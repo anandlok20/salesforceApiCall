5d73d8bb7d14460525536c2b258a6ba183d96aef
Anand Lok <imanandlok@gmail.com>
2019-12-16 19:34:53 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TraineeComponent } from './trainee/trainee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'trainee',component:TraineeComponent},
+  {path:'',redirectTo:'/trainee',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/traineeSchema.ts
@@ -0,0 +1,6 @@
+export class traineeSchema{
+    Name;
+    MobilePhone;
+    Email;
+    Birthdate;
+}
