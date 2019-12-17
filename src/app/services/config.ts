4548813b8063475997000ec7d64c49d0049ec4a5
Anand Lok <imanandlok@gmail.com>
2019-12-17 09:01:13 +0530

    emb

---
+++ src/app/services/config.ts
@@ -0,0 +1,6 @@
+export interface config {
+    access_token: string;
+    instance_url: string;
+    id: string;
+    token_type: string;
+  }
--- src/app/services/trainee.service.ts
+++ src/app/services/trainee.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
-import { HttpClient, HttpRequest, HttpHandler } from "@angular/common/http";
+import { HttpClient, HttpRequest, HttpHandler, HttpResponse, HttpHeaders } from "@angular/common/http";
+import { config } from './config';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +16,18 @@ export class TraineeService {
     client_id: '3MVG9n_HvETGhr3BO1H9Ni1aKEHTCDIIRtLjdbhlcFha2PAK.owCr3D0ChczN5iqwJekWv3tQ_pDqLjE8jjGv',
     client_secret: '0D9F82A12D821E1A2C6168462CAA5A15D3C56BDBE53644D0B632A9B977FB3C1C',
   };
-  d1: string;
+  d1: any;
   constructor(private httpClient: HttpClient, private route: Router) {
-const res=this.validUser();
-console.log(res);
+    const header=new HttpHeaders();
+
+    this.httpClient.get<config>('https://login.salesforce.com/services/oauth2/token', {headers:this.data}).subscribe(data1 => {
+      this.d1 = data1;
+  })
+
+
+    // const res1 = this.validUser();
+    // console.log('res1',res1);
+    // console.log('d1',this.d1)
 
     // const headers = new HttpHeaders({ Authorization: 'Basic' + btoa(u.username + ':'+ u.password) });
 
@@ -34,12 +45,31 @@ console.log(res);
 
     //   );
 
+    // return this.http.get<any>(this.baseurl+"/validate?username="+u.username+"&"+"password="+u.password,{ headers }).pipe(
+    //   map(
+    //     userData => {
+    //       sessionStorage.setItem('username', u.username);
+    //       let authString = 'Basic ' + btoa(u.username + ':' + u.password);
+    //       sessionStorage.setItem('basicauth', authString);
+    //       console.log("userdata"+userData);
+    //       return userData;
+    //     }
+    //   )
+
+    // );
 
   }
 
-  validUser(){
-    return this.httpClient.post<access_token:access_token>('https://login.salesforce.com/services/oauth2/token', this.data);
-  }
+  // validUser() {
+  //   return this.httpClient.post<config>('https://login.salesforce.com/services/oauth2/token', this.data).pipe(map(
+  //     res => {
+  //       this.d1=res;
+  //       sessionStorage.setItem('access_token',this.d1)
+  //       console.log('res', res);
+  //       return res;
+  //     }
+  //   ));
+  // }
 
 
 }
